37170045fa27dd34baf45e30961b0d4c7c0849ec
douglas86 <douglasmaxton@gmail.com>
2022-08-08 10:55:05 +0200

    Created my connection for postgres db

---
+++ server/lib/pool.js
@@ -0,0 +1,10 @@
+import pkg from "pg";
+import { keys } from "./keys.js";
+
+const { Pool } = pkg;
+const { db } = keys;
+
+export const pool = new Pool({
+  connectionString: db,
+  ssl: { rejectUnauthorized: false },
+});
