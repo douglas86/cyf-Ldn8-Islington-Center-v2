5ee0aa682bc3d8a88c08075773a1f504f1f15a9c
douglas86 <douglasmaxton@gmail.com>
2022-08-09 10:22:57 +0200

    Created the Heading page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import MarkDown from "markdown-to-jsx";
+
+import { toHeading } from "../components/atom/toHeading";
+
 const Home = () => {
+  const [landing, setLanding] = useState("");
+  const file_name = "landing.md";
+
+  useEffect(() => {
+    import(`../markdown/${file_name}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setLanding(res));
+      })
+      .catch((error) => console.log(error.message));
+  });
+
   return (
     <>
-      <h1>Home</h1>
+      <div className={styles.showcase}>
+        <div className={styles.container}>
+          {toHeading("Islington Center for Refugess and Migrants")}
+          <p className={`lead ${styles.hide_on_small}`}>
+            Welcome and supporting Refugees, Asylum seekers, and migrants in our
+            community
+          </p>
+        </div>
+      </div>
+      <div className={styles.home_content}>
+        <MarkDown>{landing}</MarkDown>
+      </div>
     </>
   );
 };
