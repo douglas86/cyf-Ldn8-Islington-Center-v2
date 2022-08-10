66260c2c62fc616ea5a01208621395654bedbd20
douglas86 <douglasmaxton@gmail.com>
2022-08-10 19:00:11 +0200

    Updated the .gitignore and server.js file

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ const { PORT } = keys;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 // routes
 app.use("/api", homeRoutes);
