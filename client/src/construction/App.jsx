2009e5e60e2ad2ff5dff0c979872dcdece2ebdaf
douglas86 <douglasmaxton@gmail.com>
2022-08-10 17:03:17 +0200

    Deleted res.send and placed res.json instead.

--- client/src/construction/App.jsx
+++ client/src/construction/App.jsx
@@ -5,8 +5,11 @@ import { routes } from "./routes";
 import Layout from "../components/template/Layout";
 
 import "./App.css";
+import useFetch from "../hooks/useFetch";
 
 const App = () => {
+  const data = useFetch("/");
+  console.log("data", data);
   const routeComponents = routes.map(({ path, component, protect }, key) => (
     <Route path={path} element={component} key={key} />
   ));
--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "Hello World" });
 });
 
 export default router;
